d3fc261bf6edb9c0b6f93076df300b011a83f726
xev <git@schore.org>
2023-05-17 18:40:56 -0400

    fix(docs): test changes

--- tests/test_chat.py
+++ tests/test_chat.py
@@ -5,4 +5,4 @@ import os
 def test_chats():
     database = imessagedb.DB(os.path.join(os.path.dirname(__file__), "chat.db"))
 
-    assert len(database.chats) == 2
+    assert len(database.chats) == 2, "Unexpected number of chats"
--- tests/test_handle.py
+++ tests/test_handle.py
@@ -5,11 +5,11 @@ import os
 def test_handles():
     database = imessagedb.DB(os.path.join(os.path.dirname(__file__), "chat.db"))
 
-    assert len(database.handles) == 2
+    assert len(database.handles) == 2, "Unexpected number of handles"
 
 
 def test_handle():
     database = imessagedb.DB(os.path.join(os.path.dirname(__file__), "chat.db"))
     h = database.handles
     hand = h.handles[2]
-    assert hand.number == "scripting@schore.org"
+    assert hand.number == "scripting@schore.org", "Unexpected handle"
