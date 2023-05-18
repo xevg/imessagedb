2b638d0a6bd10286441bc50fe37e9f91cfe204b5
xev <git@schore.org>
2023-05-17 21:00:43 -0400

    test(attachment): fixed attachment test to run on github

--- tests/test_attachments.py
+++ tests/test_attachments.py
@@ -12,7 +12,7 @@ def test_attachments():
 def test_attachment():
     database = imessagedb.DB(os.path.join(os.path.dirname(__file__), "chat.db"))
 
-    attachments = database.attachment_list
-    attachment = attachments.attachment_list[98368] 
+    attachments = database.attachment_list 
+    attachment = attachments.attachment_list[98368]
     expected_path = f"{os.environ['HOME']}/Library/Messages/Attachments/4f/15/D7CEBAED-9844-4B25-B841-F7748EA3BCAD/IMG_4911.heic"
     assert attachment.original_path == expected_path, "Unexpected value in attachment"
